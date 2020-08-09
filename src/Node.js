3ededfa99df66822ae0df7dd86d1b8561edb08ea
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-08-09 15:54:02 +0200

    :books: docs(Node): Add documentation.

--- src/Node.js
+++ src/Node.js
@@ -1,3 +1,8 @@
+/**
+ * Base node class.
+ *
+ * @param {Object} value - The value to hold.
+ */
 export default function Node(value) {
 	this.value = value; // Key
 	this.prev = this; // Pointer to previous (left) sibling
