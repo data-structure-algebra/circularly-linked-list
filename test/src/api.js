996c53c72bdace086566330c221b7e2e3801e47f
Aurélien Ooms <aurelien.ooms@gmail.com>
2020-10-19 11:20:38 +0200

    :unamused: chore(dev-deps): Upgrade xo.

--- test/src/api.js
+++ test/src/api.js
@@ -9,43 +9,43 @@ const values = (first) => list(map((x) => x.value, iter(first)));
 test('API', (t) => {
 	let A = empty();
 
-	t.deepEqual([], values(A));
+	t.deepEqual(values(A), []);
 
 	t.throws(() => pop(A), {message: /empty/});
 	t.throws(() => shift(A), {message: /empty/});
 
 	A = push(A, 1);
 
-	t.deepEqual([1], values(A));
+	t.deepEqual(values(A), [1]);
 
 	A = unshift(A, 2);
 
-	t.deepEqual([2, 1], values(A));
+	t.deepEqual(values(A), [2, 1]);
 
 	let B = from([4, 3]);
 
-	t.deepEqual([4, 3], values(B));
+	t.deepEqual(values(B), [4, 3]);
 
 	B = concat(A, B);
 	A = empty();
 
-	t.deepEqual([2, 1, 4, 3], values(B));
-	t.deepEqual([], values(A));
+	t.deepEqual(values(B), [2, 1, 4, 3]);
+	t.deepEqual(values(A), []);
 
 	B = concat(A, B);
-	t.deepEqual([2, 1, 4, 3], values(B));
+	t.deepEqual(values(B), [2, 1, 4, 3]);
 
 	B = concat(B, A);
-	t.deepEqual([2, 1, 4, 3], values(B));
+	t.deepEqual(values(B), [2, 1, 4, 3]);
 
 	A = unshift(A, 8);
-	t.deepEqual([8], values(A));
+	t.deepEqual(values(A), [8]);
 	B = push(B, 7);
-	t.deepEqual([2, 1, 4, 3, 7], values(B));
+	t.deepEqual(values(B), [2, 1, 4, 3, 7]);
 
 	A = concat(A, B);
 	B = empty();
 
-	t.deepEqual([8, 2, 1, 4, 3, 7], values(A));
-	t.deepEqual([], values(B));
+	t.deepEqual(values(A), [8, 2, 1, 4, 3, 7]);
+	t.deepEqual(values(B), []);
 });
