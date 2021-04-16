5ccbf6a72c82666f77f4019e9013352386a8df51
Aurélien Ooms <aurelien.ooms@gmail.com>
2021-04-16 21:35:32 +0200

    :sparkles: feat: Implement values and _iter_fast.
    Fixes #45 and fixes #46.

--- test/src/api.js
+++ test/src/api.js
@@ -1,6 +1,6 @@
 import test from 'ava';
 
-import {list, map} from '@aureooms/js-itertools';
+import {list} from '@aureooms/js-itertools';
 
 import {
 	empty,
@@ -10,51 +10,51 @@ import {
 	shift,
 	unshift,
 	concat,
-	iter,
+	values,
 } from '../../src/index.js';
 
-const values = (first) => list(map((x) => x.value, iter(first)));
+const toArray = (first) => list(values(first));
 
 test('API', (t) => {
 	let A = empty();
 
-	t.deepEqual(values(A), []);
+	t.deepEqual(toArray(A), []);
 
 	t.throws(() => pop(A), {message: /empty/});
 	t.throws(() => shift(A), {message: /empty/});
 
 	A = push(A, 1);
 
-	t.deepEqual(values(A), [1]);
+	t.deepEqual(toArray(A), [1]);
 
 	A = unshift(A, 2);
 
-	t.deepEqual(values(A), [2, 1]);
+	t.deepEqual(toArray(A), [2, 1]);
 
 	let B = from([4, 3]);
 
-	t.deepEqual(values(B), [4, 3]);
+	t.deepEqual(toArray(B), [4, 3]);
 
 	B = concat(A, B);
 	A = empty();
 
-	t.deepEqual(values(B), [2, 1, 4, 3]);
-	t.deepEqual(values(A), []);
+	t.deepEqual(toArray(B), [2, 1, 4, 3]);
+	t.deepEqual(toArray(A), []);
 
 	B = concat(A, B);
-	t.deepEqual(values(B), [2, 1, 4, 3]);
+	t.deepEqual(toArray(B), [2, 1, 4, 3]);
 
 	B = concat(B, A);
-	t.deepEqual(values(B), [2, 1, 4, 3]);
+	t.deepEqual(toArray(B), [2, 1, 4, 3]);
 
 	A = unshift(A, 8);
-	t.deepEqual(values(A), [8]);
+	t.deepEqual(toArray(A), [8]);
 	B = push(B, 7);
-	t.deepEqual(values(B), [2, 1, 4, 3, 7]);
+	t.deepEqual(toArray(B), [2, 1, 4, 3, 7]);
 
 	A = concat(A, B);
 	B = empty();
 
-	t.deepEqual(values(A), [8, 2, 1, 4, 3, 7]);
-	t.deepEqual(values(B), []);
+	t.deepEqual(toArray(A), [8, 2, 1, 4, 3, 7]);
+	t.deepEqual(toArray(B), []);
 });
